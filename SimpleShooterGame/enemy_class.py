aad39988e2febdf4556c2a6b61ef4250401e1876
stealthness <stevewestinlondon@gmail.com>
2024-12-17 20:32:49 +0000

    added Move

--- SimpleShooterGame/enemy_class.py
+++ SimpleShooterGame/enemy_class.py
@@ -9,6 +9,11 @@ class Solder(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.image = pygame.transform.scale(self.image, (int(self.rect.width * scale), int(self.rect.height * scale)))
+        self.speed = 2
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+    def move(self, dir):
+        self.rect.x += dir.x * self.speed
+        self.rect.y += dir.y * self.speed
--- SimpleShooterGame/main.py
+++ SimpleShooterGame/main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import Vector2
 
 from SimpleShooterGame.enemy_class import Solder
 
@@ -10,24 +11,62 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Simple Shooter Game')
 
-player = Solder(100, 100, 2)
+class Scene:
+    def __init__(self):
+        self.enemies = []
 
-enemy = Solder(200, 100, 2)
+    def add_enemy(self, enemy):
+        self.enemies.append(enemy)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+    def update(self):
+        for enemy in self.enemies:
+            enemy.draw(screen)
 
-    screen.fill((255, 255, 255))
 
-    screen.blit(player.image, player.rect)
-    screen.blit(enemy.image, enemy.rect)
+def init(scene):
 
-    pygame.display.flip()
-    pygame.display.update()
-    pygame.time.Clock().tick(60)
+    player = Solder(100, 100, 2)
+    enemy = Solder(200, 100, 2)
+    scene.add_enemy(player)
+    scene.add_enemy(enemy)
+
+def run():
+    is_running = True
+    scene = Scene()
+    init(scene)
+
+    dir = Vector2(0, 0)
+
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    is_running = False
+                if event.key == pygame.K_a:
+                    dir.x = -1
+                if event.key == pygame.K_d:
+                    dir.x = 1
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    dir.x = 0
+                if event.key == pygame.K_d:
+                    dir.x = 0
+
+        screen.fill((255, 255, 255))
+
+        scene.enemies[0].move(dir)
+
+        for enemy in scene.enemies:
+            enemy.draw(screen)
+
+        pygame.display.flip()
+        pygame.display.update()
+        pygame.time.Clock().tick(60)
 
 if __name__ == '__main__':
-    pass
+    run()
+    pygame.quit()
+    exit(0)
