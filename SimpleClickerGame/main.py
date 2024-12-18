1788249cd9d84c97fba34e4956d2063a9c291256
Stealthness <stevewestinlondon@gmail.com>
2024-12-18 02:55:31 +0000

    added background color

--- SimpleClickerGame/main.py
+++ SimpleClickerGame/main.py
@@ -4,6 +4,11 @@ import pygame
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+BACKGROUND_COLOR = (200, 200, 255)
+
+pygame.init()
+pygame.display.set_caption("Clicker Game")
+screen = pygame.display.set_mode((500, 500))
 
 
 def run():
@@ -11,13 +16,13 @@ def run():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
+        screen.fill(BACKGROUND_COLOR)
+        pygame.display.flip()
+        pygame.display.update()
 
 
 if __name__ == "__main__":
-    pygame.init()
-    pygame.display.set_caption("Clicker Game")
-    screen = pygame.display.set_mode((500, 500))
     run()
     pygame.quit()
     sys.exit()
