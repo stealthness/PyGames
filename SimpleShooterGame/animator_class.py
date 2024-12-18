594234d5bc26c5caca84fcaf1f8a3a39c8d23eda
stealthness <stevewestinlondon@gmail.com>
2024-12-18 01:29:04 +0000

    fixed bug

--- SimpleShooterGame/animator_class.py
+++ SimpleShooterGame/animator_class.py
@@ -12,17 +12,21 @@ class Animator:
         self.index = 0
         self.frame_times = frame_times
 
-
-
-    def get_image(self):
+    def check_frame_time(self):
         time_since_last_frame = pygame.time.get_ticks() - self.time_last_frame
         time_to_next_frame = self.time * 1000
         # check if frame_times is not None, otherwise set time_to_next_frame to the given frame time
         if self.frame_times is not None:
             time_to_next_frame = self.frame_times[self.index] * 1000
 
-        if time_since_last_frame > time_to_next_frame:
-            self.index = (self.index + 1) % 4
-            self.time_last_frame = pygame.time.get_ticks()
+        return time_since_last_frame > time_to_next_frame
+
+    def get_image(self):
+
+        self.index = (self.index + 1) % 4
+        self.time_last_frame = pygame.time.get_ticks()
+        print(f'index:{self.index}')
         return self.images[self.index]
 
+    def get_image_at(self, index):
+        return self.images[index]
--- SimpleShooterGame/enemy_class.py
+++ SimpleShooterGame/enemy_class.py
@@ -5,23 +5,27 @@ from SimpleShooterGame.animator_class import Animator
 
 
 class Solder(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale, speed = 2, image = 'Art/Carol.png'):
+    def __init__(self, x, y, scale, speed = 2, image = 'Art/carol/Carol-Idle.png'):
         pygame.sprite.Sprite.__init__(self)
         self.animator = Animator('Art/carol/carol_idle_', 0.2, frame_count=4, frame_times=[3, 0.2, 0.2, 0.2])
-        self.image = pygame.image.load(image)
         self.scale = scale
-        self.rect = self.image.get_rect()
+        self.default_image = self.animator.get_image_at(0)
+        self.rect = self.default_image.get_rect()
+        self.default_image = pygame.transform.scale(self.default_image,(int(self.rect.width * self.scale), int(self.rect.height * self.scale)))
         self.rect.topleft = (x, y)
         self.image = self.speed = speed
         self.dir = pygame.Vector2(0, 0)
         self.is_flip_x = True
 
     def draw(self, screen):
-        self.image = self.animator.get_image()
-        self.image = pygame.transform.scale(self.image, (int(self.rect.width * self.scale), int(self.rect.height * self.scale)))
+        if self.animator.check_frame_time():
+            self.image = self.animator.get_image()
+            self.image = pygame.transform.scale(self.image, (int(self.rect.width * self.scale), int(self.rect.height * self.scale)))
+        if isinstance(self.image, pygame.surface.Surface):
+            screen.blit(pygame.transform.flip(self.image, self.is_flip_x, False), self.rect)
 
-        screen.blit(pygame.transform.flip(self.image, self.is_flip_x, False), self.rect)
-
+        else:
+            screen.blit(pygame.transform.flip(self.default_image, self.is_flip_x, False), self.rect)
 
     def move(self, dir):
         dx = dir.x * self.speed
