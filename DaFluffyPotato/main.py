4b72fc1de77bc71ded57e4a6283c7a913086b545
Stealthness <stevewestinlondon@gmail.com>
2024-12-19 04:37:36 +0000

    wrapped into game class

--- DaFluffyPotato/main.py
+++ DaFluffyPotato/main.py
@@ -1,25 +1,32 @@
 import sys
+
 import pygame
 
 
-pygame.init()
-screen = pygame.display.set_mode((640, 480))
-pygame.display.set_caption('DaFluffyPotato Tutorial')
-clock = pygame.time.Clock()
-FPS = 60
-BACKGROUND_COLOR = (200, 200, 250)
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.BACKGROUND_Color = (200, 200, 250)
+        self.screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption('DaFluffyPotato Tutorial')
+        self.clock = pygame.time.Clock()
+        self.FPS = 60
 
-while True:
-    screen.fill(BACKGROUND_COLOR)
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        pygame.init()
+            self.screen.fill(self.BACKGROUND_Color)
+            pygame.display.flip()
+            self.clock.tick(self.FPS)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                pygame.init()
 
-    pygame.display.flip()
-    clock.tick(FPS)
+if __name__ == '__main__':
+    game = Game()
+    game.run()
