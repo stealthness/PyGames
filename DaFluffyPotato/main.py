40221ee0dec4e6e4d1be1b647713011191f06d19
Stealthness <stevewestinlondon@gmail.com>
2024-12-19 04:29:48 +0000

    updated gitignore added resourse from https://dafluffypotato.com/assets/pg_tutorial (not added to github)

--- DaFluffyPotato/main.py
+++ DaFluffyPotato/main.py
@@ -1,18 +1,25 @@
 import sys
-
 import pygame
 
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('DaFluffyPotato Tutorial')
-
+clock = pygame.time.Clock()
+FPS = 60
+BACKGROUND_COLOR = (200, 200, 250)
 
 while True:
-    screen.fill((200, 200, 255))
+    screen.fill(BACKGROUND_COLOR)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                pygame.init()
 
     pygame.display.flip()
+    clock.tick(FPS)
