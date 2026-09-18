773908e3d9cea84c915b849772bb9b47f1a69f1d
Stealthness <stevewestinlondon@gmail.com>
2026-09-18 09:15:20 +0100

    scalling human character

--- Game Jams/[Trijam 390] Hunt the Human/config.py
+++ Game Jams/[Trijam 390] Hunt the Human/config.py
@@ -3,9 +3,14 @@ import pygame
 
 SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 600
+ART_SCALE = 4
 
 FPS = 60
 
 game_background = pygame.image.load('Hidden/Art/Backgrounds/game_background.png')
 
-human_default_image = pygame.image.load('Hidden/Art/Human/deserted_islander1.png')
+_human_base_image = pygame.image.load('Hidden/Art/Human/deserted_islander1.png')
+human_default_image = pygame.transform.scale(
+	_human_base_image,
+	(_human_base_image.get_width() * ART_SCALE, _human_base_image.get_height() * ART_SCALE),
+)
